f86620aa84e570be6e70aa0fb3bf0a56759726ad
Lonnie Souder <souderl9@students.rowan.edu>
2017-03-09 04:37:50 -0500

    Finished data path and data path TB

--- Top_Level_ModelSim_v.v
+++ Top_Level_ModelSim_v.v
@@ -13,8 +13,8 @@
 // applicable agreement for further details.
 
 // PROGRAM		"Quartus II 64-Bit"
-// VERSION		"Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Web Edition"
-// CREATED		"Wed Mar 08 19:33:05 2017"
+// VERSION		"Version 13.1.0 Build 162 10/23/2013 SJ Web Edition"
+// CREATED		"Thu Mar 09 02:29:43 2017"
 
 module Top_Level_ModelSim_v(
 	clock_50,
@@ -31,7 +31,10 @@ module Top_Level_ModelSim_v(
 	k,
 	MD,
 	PS,
+	SS,
 	Cout,
+	Z,
+	N,
 	R0,
 	R1,
 	R2,
@@ -55,9 +58,12 @@ input wire	[2:0] BA;
 input wire	[2:0] DA;
 input wire	[4:0] FS;
 input wire	[15:0] k;
-input wire	[1:0] MD;
+input wire	[4:0] MD;
 input wire	[1:0] PS;
+input wire	[1:0] SS;
 output wire	Cout;
+output wire	Z;
+output wire	N;
 output wire	[15:0] R0;
 output wire	[15:0] R1;
 output wire	[15:0] R2;
@@ -70,117 +76,129 @@ output wire	[15:0] R7;
 wire	[15:0] B;
 wire	[15:0] gdfx_temp0;
 wire	[15:0] PC_out;
+wire	[2:0] StS;
 wire	[15:0] SYNTHESIZED_WIRE_0;
-wire	SYNTHESIZED_WIRE_1;
+wire	[15:0] SYNTHESIZED_WIRE_1;
 wire	[15:0] SYNTHESIZED_WIRE_2;
-wire	SYNTHESIZED_WIRE_3;
-wire	[15:0] SYNTHESIZED_WIRE_12;
-wire	SYNTHESIZED_WIRE_5;
+wire	[15:0] SYNTHESIZED_WIRE_3;
+wire	[15:0] SYNTHESIZED_WIRE_4;
+wire	[15:0] SYNTHESIZED_WIRE_5;
+wire	SYNTHESIZED_WIRE_6;
 wire	[15:0] SYNTHESIZED_WIRE_7;
-wire	[15:0] SYNTHESIZED_WIRE_8;
-wire	SYNTHESIZED_WIRE_9;
-wire	[15:0] SYNTHESIZED_WIRE_10;
-wire	SYNTHESIZED_WIRE_11;
 
 
 
 
 
-decoder2to4	b2v_inst1(
-	.select(MD),
-	.o0(SYNTHESIZED_WIRE_3),
-	.o1(SYNTHESIZED_WIRE_9),
-	.o2(SYNTHESIZED_WIRE_5),
-	.o3(SYNTHESIZED_WIRE_1));
+stack	b2v_inst(
+	.clk(clock_50),
+	.reset(StS[2]),
+	.push(StS[1]),
+	.pop(StS[0]),
+	.data(gdfx_temp0),
+	.out(SYNTHESIZED_WIRE_7));
+	defparam	b2v_inst.depth = 256;
+	defparam	b2v_inst.width = 16;
 
-assign	gdfx_temp0[15] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[15] : 1'bz;
-assign	gdfx_temp0[14] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[14] : 1'bz;
-assign	gdfx_temp0[13] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[13] : 1'bz;
-assign	gdfx_temp0[12] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[12] : 1'bz;
-assign	gdfx_temp0[11] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[11] : 1'bz;
-assign	gdfx_temp0[10] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[10] : 1'bz;
-assign	gdfx_temp0[9] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[9] : 1'bz;
-assign	gdfx_temp0[8] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[8] : 1'bz;
-assign	gdfx_temp0[7] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[7] : 1'bz;
-assign	gdfx_temp0[6] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[6] : 1'bz;
-assign	gdfx_temp0[5] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[5] : 1'bz;
-assign	gdfx_temp0[4] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[4] : 1'bz;
-assign	gdfx_temp0[3] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[3] : 1'bz;
-assign	gdfx_temp0[2] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[2] : 1'bz;
-assign	gdfx_temp0[1] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[1] : 1'bz;
-assign	gdfx_temp0[0] = SYNTHESIZED_WIRE_1 ? SYNTHESIZED_WIRE_0[0] : 1'bz;
 
-assign	gdfx_temp0[15] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[15] : 1'bz;
-assign	gdfx_temp0[14] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[14] : 1'bz;
-assign	gdfx_temp0[13] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[13] : 1'bz;
-assign	gdfx_temp0[12] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[12] : 1'bz;
-assign	gdfx_temp0[11] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[11] : 1'bz;
-assign	gdfx_temp0[10] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[10] : 1'bz;
-assign	gdfx_temp0[9] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[9] : 1'bz;
-assign	gdfx_temp0[8] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[8] : 1'bz;
-assign	gdfx_temp0[7] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[7] : 1'bz;
-assign	gdfx_temp0[6] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[6] : 1'bz;
-assign	gdfx_temp0[5] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[5] : 1'bz;
-assign	gdfx_temp0[4] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[4] : 1'bz;
-assign	gdfx_temp0[3] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[3] : 1'bz;
-assign	gdfx_temp0[2] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[2] : 1'bz;
-assign	gdfx_temp0[1] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[1] : 1'bz;
-assign	gdfx_temp0[0] = SYNTHESIZED_WIRE_3 ? SYNTHESIZED_WIRE_2[0] : 1'bz;
+stack_mux	b2v_inst1(
+	.SS(SS),
+	.out(StS));
 
-assign	gdfx_temp0[15] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[15] : 1'bz;
-assign	gdfx_temp0[14] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[14] : 1'bz;
-assign	gdfx_temp0[13] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[13] : 1'bz;
-assign	gdfx_temp0[12] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[12] : 1'bz;
-assign	gdfx_temp0[11] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[11] : 1'bz;
-assign	gdfx_temp0[10] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[10] : 1'bz;
-assign	gdfx_temp0[9] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[9] : 1'bz;
-assign	gdfx_temp0[8] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[8] : 1'bz;
-assign	gdfx_temp0[7] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[7] : 1'bz;
-assign	gdfx_temp0[6] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[6] : 1'bz;
-assign	gdfx_temp0[5] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[5] : 1'bz;
-assign	gdfx_temp0[4] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[4] : 1'bz;
-assign	gdfx_temp0[3] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[3] : 1'bz;
-assign	gdfx_temp0[2] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[2] : 1'bz;
-assign	gdfx_temp0[1] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[1] : 1'bz;
-assign	gdfx_temp0[0] = SYNTHESIZED_WIRE_5 ? SYNTHESIZED_WIRE_12[0] : 1'bz;
+
+status	b2v_inst10(
+	.data(gdfx_temp0),
+	.Z(Z),
+	.N(N));
+
+assign	gdfx_temp0[15] = MD[0] ? SYNTHESIZED_WIRE_0[15] : 1'bz;
+assign	gdfx_temp0[14] = MD[0] ? SYNTHESIZED_WIRE_0[14] : 1'bz;
+assign	gdfx_temp0[13] = MD[0] ? SYNTHESIZED_WIRE_0[13] : 1'bz;
+assign	gdfx_temp0[12] = MD[0] ? SYNTHESIZED_WIRE_0[12] : 1'bz;
+assign	gdfx_temp0[11] = MD[0] ? SYNTHESIZED_WIRE_0[11] : 1'bz;
+assign	gdfx_temp0[10] = MD[0] ? SYNTHESIZED_WIRE_0[10] : 1'bz;
+assign	gdfx_temp0[9] = MD[0] ? SYNTHESIZED_WIRE_0[9] : 1'bz;
+assign	gdfx_temp0[8] = MD[0] ? SYNTHESIZED_WIRE_0[8] : 1'bz;
+assign	gdfx_temp0[7] = MD[0] ? SYNTHESIZED_WIRE_0[7] : 1'bz;
+assign	gdfx_temp0[6] = MD[0] ? SYNTHESIZED_WIRE_0[6] : 1'bz;
+assign	gdfx_temp0[5] = MD[0] ? SYNTHESIZED_WIRE_0[5] : 1'bz;
+assign	gdfx_temp0[4] = MD[0] ? SYNTHESIZED_WIRE_0[4] : 1'bz;
+assign	gdfx_temp0[3] = MD[0] ? SYNTHESIZED_WIRE_0[3] : 1'bz;
+assign	gdfx_temp0[2] = MD[0] ? SYNTHESIZED_WIRE_0[2] : 1'bz;
+assign	gdfx_temp0[1] = MD[0] ? SYNTHESIZED_WIRE_0[1] : 1'bz;
+assign	gdfx_temp0[0] = MD[0] ? SYNTHESIZED_WIRE_0[0] : 1'bz;
+
+assign	gdfx_temp0[15] = MD[3] ? SYNTHESIZED_WIRE_1[15] : 1'bz;
+assign	gdfx_temp0[14] = MD[3] ? SYNTHESIZED_WIRE_1[14] : 1'bz;
+assign	gdfx_temp0[13] = MD[3] ? SYNTHESIZED_WIRE_1[13] : 1'bz;
+assign	gdfx_temp0[12] = MD[3] ? SYNTHESIZED_WIRE_1[12] : 1'bz;
+assign	gdfx_temp0[11] = MD[3] ? SYNTHESIZED_WIRE_1[11] : 1'bz;
+assign	gdfx_temp0[10] = MD[3] ? SYNTHESIZED_WIRE_1[10] : 1'bz;
+assign	gdfx_temp0[9] = MD[3] ? SYNTHESIZED_WIRE_1[9] : 1'bz;
+assign	gdfx_temp0[8] = MD[3] ? SYNTHESIZED_WIRE_1[8] : 1'bz;
+assign	gdfx_temp0[7] = MD[3] ? SYNTHESIZED_WIRE_1[7] : 1'bz;
+assign	gdfx_temp0[6] = MD[3] ? SYNTHESIZED_WIRE_1[6] : 1'bz;
+assign	gdfx_temp0[5] = MD[3] ? SYNTHESIZED_WIRE_1[5] : 1'bz;
+assign	gdfx_temp0[4] = MD[3] ? SYNTHESIZED_WIRE_1[4] : 1'bz;
+assign	gdfx_temp0[3] = MD[3] ? SYNTHESIZED_WIRE_1[3] : 1'bz;
+assign	gdfx_temp0[2] = MD[3] ? SYNTHESIZED_WIRE_1[2] : 1'bz;
+assign	gdfx_temp0[1] = MD[3] ? SYNTHESIZED_WIRE_1[1] : 1'bz;
+assign	gdfx_temp0[0] = MD[3] ? SYNTHESIZED_WIRE_1[0] : 1'bz;
+
+assign	gdfx_temp0[15] = MD[1] ? PC_out[15] : 1'bz;
+assign	gdfx_temp0[14] = MD[1] ? PC_out[14] : 1'bz;
+assign	gdfx_temp0[13] = MD[1] ? PC_out[13] : 1'bz;
+assign	gdfx_temp0[12] = MD[1] ? PC_out[12] : 1'bz;
+assign	gdfx_temp0[11] = MD[1] ? PC_out[11] : 1'bz;
+assign	gdfx_temp0[10] = MD[1] ? PC_out[10] : 1'bz;
+assign	gdfx_temp0[9] = MD[1] ? PC_out[9] : 1'bz;
+assign	gdfx_temp0[8] = MD[1] ? PC_out[8] : 1'bz;
+assign	gdfx_temp0[7] = MD[1] ? PC_out[7] : 1'bz;
+assign	gdfx_temp0[6] = MD[1] ? PC_out[6] : 1'bz;
+assign	gdfx_temp0[5] = MD[1] ? PC_out[5] : 1'bz;
+assign	gdfx_temp0[4] = MD[1] ? PC_out[4] : 1'bz;
+assign	gdfx_temp0[3] = MD[1] ? PC_out[3] : 1'bz;
+assign	gdfx_temp0[2] = MD[1] ? PC_out[2] : 1'bz;
+assign	gdfx_temp0[1] = MD[1] ? PC_out[1] : 1'bz;
+assign	gdfx_temp0[0] = MD[1] ? PC_out[0] : 1'bz;
 
 
 IR	b2v_inst14(
 	.CLK(clock_50),
 	.L(IR_L),
-	.in(SYNTHESIZED_WIRE_12),
+	.in(SYNTHESIZED_WIRE_2),
 	.out(SYNTHESIZED_WIRE_0));
 
 
 rom_case	b2v_inst15(
 	.address(PC_out[7:0]),
-	.out(SYNTHESIZED_WIRE_12));
+	.out(SYNTHESIZED_WIRE_2));
 
 
 ALU_16bit	b2v_inst2(
 	.Cin(Cin),
-	.A(SYNTHESIZED_WIRE_7),
+	.A(SYNTHESIZED_WIRE_3),
 	.B(B),
 	.FS(FS),
 	.Cout(Cout),
-	.F(SYNTHESIZED_WIRE_8));
+	.F(SYNTHESIZED_WIRE_4));
 
-assign	gdfx_temp0[15] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[15] : 1'bz;
-assign	gdfx_temp0[14] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[14] : 1'bz;
-assign	gdfx_temp0[13] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[13] : 1'bz;
-assign	gdfx_temp0[12] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[12] : 1'bz;
-assign	gdfx_temp0[11] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[11] : 1'bz;
-assign	gdfx_temp0[10] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[10] : 1'bz;
-assign	gdfx_temp0[9] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[9] : 1'bz;
-assign	gdfx_temp0[8] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[8] : 1'bz;
-assign	gdfx_temp0[7] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[7] : 1'bz;
-assign	gdfx_temp0[6] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[6] : 1'bz;
-assign	gdfx_temp0[5] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[5] : 1'bz;
-assign	gdfx_temp0[4] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[4] : 1'bz;
-assign	gdfx_temp0[3] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[3] : 1'bz;
-assign	gdfx_temp0[2] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[2] : 1'bz;
-assign	gdfx_temp0[1] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[1] : 1'bz;
-assign	gdfx_temp0[0] = SYNTHESIZED_WIRE_9 ? SYNTHESIZED_WIRE_8[0] : 1'bz;
+assign	gdfx_temp0[15] = MD[2] ? SYNTHESIZED_WIRE_4[15] : 1'bz;
+assign	gdfx_temp0[14] = MD[2] ? SYNTHESIZED_WIRE_4[14] : 1'bz;
+assign	gdfx_temp0[13] = MD[2] ? SYNTHESIZED_WIRE_4[13] : 1'bz;
+assign	gdfx_temp0[12] = MD[2] ? SYNTHESIZED_WIRE_4[12] : 1'bz;
+assign	gdfx_temp0[11] = MD[2] ? SYNTHESIZED_WIRE_4[11] : 1'bz;
+assign	gdfx_temp0[10] = MD[2] ? SYNTHESIZED_WIRE_4[10] : 1'bz;
+assign	gdfx_temp0[9] = MD[2] ? SYNTHESIZED_WIRE_4[9] : 1'bz;
+assign	gdfx_temp0[8] = MD[2] ? SYNTHESIZED_WIRE_4[8] : 1'bz;
+assign	gdfx_temp0[7] = MD[2] ? SYNTHESIZED_WIRE_4[7] : 1'bz;
+assign	gdfx_temp0[6] = MD[2] ? SYNTHESIZED_WIRE_4[6] : 1'bz;
+assign	gdfx_temp0[5] = MD[2] ? SYNTHESIZED_WIRE_4[5] : 1'bz;
+assign	gdfx_temp0[4] = MD[2] ? SYNTHESIZED_WIRE_4[4] : 1'bz;
+assign	gdfx_temp0[3] = MD[2] ? SYNTHESIZED_WIRE_4[3] : 1'bz;
+assign	gdfx_temp0[2] = MD[2] ? SYNTHESIZED_WIRE_4[2] : 1'bz;
+assign	gdfx_temp0[1] = MD[2] ? SYNTHESIZED_WIRE_4[1] : 1'bz;
+assign	gdfx_temp0[0] = MD[2] ? SYNTHESIZED_WIRE_4[0] : 1'bz;
 
 
 RAM1	b2v_inst3(
@@ -188,7 +206,7 @@ RAM1	b2v_inst3(
 	.wren(MW),
 	.address(B[8:0]),
 	.data(gdfx_temp0),
-	.q(SYNTHESIZED_WIRE_2));
+	.q(SYNTHESIZED_WIRE_1));
 
 
 RegisterFile	b2v_inst4(
@@ -199,8 +217,8 @@ RegisterFile	b2v_inst4(
 	.BA(BA),
 	.D(gdfx_temp0),
 	.DA(DA),
-	.A(SYNTHESIZED_WIRE_7),
-	.B(SYNTHESIZED_WIRE_10),
+	.A(SYNTHESIZED_WIRE_3),
+	.B(SYNTHESIZED_WIRE_5),
 	.R0(R0),
 	.R1(R1),
 	.R2(R2),
@@ -217,22 +235,22 @@ PC	b2v_inst5(
 	.PS(PS),
 	.out(PC_out));
 
-assign	B[15] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[15] : 1'bz;
-assign	B[14] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[14] : 1'bz;
-assign	B[13] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[13] : 1'bz;
-assign	B[12] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[12] : 1'bz;
-assign	B[11] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[11] : 1'bz;
-assign	B[10] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[10] : 1'bz;
-assign	B[9] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[9] : 1'bz;
-assign	B[8] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[8] : 1'bz;
-assign	B[7] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[7] : 1'bz;
-assign	B[6] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[6] : 1'bz;
-assign	B[5] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[5] : 1'bz;
-assign	B[4] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[4] : 1'bz;
-assign	B[3] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[3] : 1'bz;
-assign	B[2] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[2] : 1'bz;
-assign	B[1] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[1] : 1'bz;
-assign	B[0] = SYNTHESIZED_WIRE_11 ? SYNTHESIZED_WIRE_10[0] : 1'bz;
+assign	B[15] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[15] : 1'bz;
+assign	B[14] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[14] : 1'bz;
+assign	B[13] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[13] : 1'bz;
+assign	B[12] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[12] : 1'bz;
+assign	B[11] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[11] : 1'bz;
+assign	B[10] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[10] : 1'bz;
+assign	B[9] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[9] : 1'bz;
+assign	B[8] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[8] : 1'bz;
+assign	B[7] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[7] : 1'bz;
+assign	B[6] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[6] : 1'bz;
+assign	B[5] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[5] : 1'bz;
+assign	B[4] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[4] : 1'bz;
+assign	B[3] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[3] : 1'bz;
+assign	B[2] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[2] : 1'bz;
+assign	B[1] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[1] : 1'bz;
+assign	B[0] = SYNTHESIZED_WIRE_6 ? SYNTHESIZED_WIRE_5[0] : 1'bz;
 
 assign	B[15] = MA ? k[15] : 1'bz;
 assign	B[14] = MA ? k[14] : 1'bz;
@@ -251,7 +269,24 @@ assign	B[2] = MA ? k[2] : 1'bz;
 assign	B[1] = MA ? k[1] : 1'bz;
 assign	B[0] = MA ? k[0] : 1'bz;
 
-assign	SYNTHESIZED_WIRE_11 =  ~MA;
+assign	SYNTHESIZED_WIRE_6 =  ~MA;
+
+assign	gdfx_temp0[15] = MD[4] ? SYNTHESIZED_WIRE_7[15] : 1'bz;
+assign	gdfx_temp0[14] = MD[4] ? SYNTHESIZED_WIRE_7[14] : 1'bz;
+assign	gdfx_temp0[13] = MD[4] ? SYNTHESIZED_WIRE_7[13] : 1'bz;
+assign	gdfx_temp0[12] = MD[4] ? SYNTHESIZED_WIRE_7[12] : 1'bz;
+assign	gdfx_temp0[11] = MD[4] ? SYNTHESIZED_WIRE_7[11] : 1'bz;
+assign	gdfx_temp0[10] = MD[4] ? SYNTHESIZED_WIRE_7[10] : 1'bz;
+assign	gdfx_temp0[9] = MD[4] ? SYNTHESIZED_WIRE_7[9] : 1'bz;
+assign	gdfx_temp0[8] = MD[4] ? SYNTHESIZED_WIRE_7[8] : 1'bz;
+assign	gdfx_temp0[7] = MD[4] ? SYNTHESIZED_WIRE_7[7] : 1'bz;
+assign	gdfx_temp0[6] = MD[4] ? SYNTHESIZED_WIRE_7[6] : 1'bz;
+assign	gdfx_temp0[5] = MD[4] ? SYNTHESIZED_WIRE_7[5] : 1'bz;
+assign	gdfx_temp0[4] = MD[4] ? SYNTHESIZED_WIRE_7[4] : 1'bz;
+assign	gdfx_temp0[3] = MD[4] ? SYNTHESIZED_WIRE_7[3] : 1'bz;
+assign	gdfx_temp0[2] = MD[4] ? SYNTHESIZED_WIRE_7[2] : 1'bz;
+assign	gdfx_temp0[1] = MD[4] ? SYNTHESIZED_WIRE_7[1] : 1'bz;
+assign	gdfx_temp0[0] = MD[4] ? SYNTHESIZED_WIRE_7[0] : 1'bz;
 
 
 endmodule
---
+++ Top_Level_ModelSim_v_t.v
@@ -0,0 +1,423 @@
+module Top_Level_ModelSim_v_t;
+	reg clock_50_t;
+	reg WR_t;
+	reg clear_t;
+	reg MW_t;
+	reg MA_t;
+	reg IR_L_t;
+	reg Cin_t;
+	reg [2:0] AA_t;
+	reg [2:0] BA_t;
+	reg [2:0] DA_t;
+	reg [4:0] FS_t;
+	reg [15:0] k_t;
+	reg [4:0 MD_t;
+	reg [1:0]PS_t;
+	reg [1:0] SS_t;
+	wire Cout_t;
+	wire Z_t;
+	wire N_t;
+	wire [15:0] R0_t;
+	wire [15:0] R1_t;
+	wire [15:0] R2_t;
+	wire [15:0] R3_t;
+	wire [15:0] R4_t;
+	wire [15:0] R5_t;
+	wire [15:0] R6_t;
+	wire [15:0] R7_t;
+	
+	Top_Level_ModelSim_v dut(
+	.clock_50(clock_50_t),
+	.WR(WR_t),
+	.clear(clear_t),
+	.MW(MW_t),
+	.MA(MA_t),
+	.IR_L(IR_L_t),
+	.Cin(Cin_t),
+	.AA(AA_t),
+	.BA(BA_t),
+	.DA(DA_t),
+	.FS(FS_t),
+	.k(k_t),
+	.MD(MD_t),
+	.PS(PS_t),
+	.SS(SS_t),
+	.Cout(Cout_t),
+	.Z(Z_t),
+	.N(N_t),
+	.R0(R0_t),
+	.R1(R1_t),
+	.R2(R2_t),
+	.R3(R3_t),
+	.R4(R4_t),
+	.R5(R5_t),
+	.R6(R6_t),
+	.R7(R7_t)
+	);
+	
+	initial begin
+		clock_50_t <= 1'b0;
+		clear_t <= 1'b1;
+		forever
+		#10 clock_t = ~clock_t;
+	end 
+	
+	//**********Begin test**************
+	initial begin
+	#1	
+	//NOP
+	PS_t		<=	2'b00;
+	IR_L_t	<=	0;
+	AA_t		<=	3'b000;
+	BA_t		<=	3'b000;
+	DA_t		<=	3'b000;
+	WR_t		<=	0;
+	clear_t	<=	0;
+	FS_t		<=	5'b00000;
+	Cin_t		<=	0;
+	MD_t		<=	5'b00000;
+	MA_t		<=	0;
+	k_t		<=	4'h0000;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//LRI
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	DA_t		<=	3'b000;
+	WR_t		<=	1;
+	FS_t		<=	5'b01010;
+	MD_t		<=	5'b00100;
+	MA_t		<=	1;
+	k_t		<=	4'h000f;
+	
+	#1
+	//INC
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b000;
+	DA_t		<=	3'b001;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b10010;
+	Cin_t		<=	0;
+	MD_t		<=	5'b00100;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//ADD
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b000;
+	BA_t		<=	3'b001;
+	DA_t		<=	3'b010;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b10100;
+	Cin_t		<=	0;
+	MD_t		<=	5'b00100;
+	MA_t		<=	0;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//SUB
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b001;
+	BA_t		<=	3'b000;
+	DA_t		<=	3'b011;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b10110;
+	Cin_t		<=	1;
+	MD_t		<=	5'b00100;
+	MA_t		<=	0;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//DEC
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b001;
+	DA_t		<=	3'b100;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b10110;
+	MD_t		<=	5'b00100;
+	MA_t		<=	1;
+	k_t		<=	4'h0001;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//NEG
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b000;
+	DA_t		<=	3'b000;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b10011;
+	Cin_t		<=	1;
+	MD_t		<=	5'b00100;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//SHR and PUSH
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	BA_t		<=	3'b000;
+	WR_t		<=	0;
+	clear_t	<=	0;
+	FS_t		<=	5'b11001;
+	MD_t		<=	5'b00100;
+	MA_t		<=	0;
+	MW_t		<=	0;
+	SS_t		<=	2'b01;
+	
+	#1
+	//SHL and PUSH
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	BA_t		<=	3'b000;
+	WR_t		<=	0;
+	clear_t	<=	0;
+	FS_t		<=	5'b11000;
+	MD_t		<=	5'b00100;
+	MA_t		<=	0;
+	MW_t		<=	0;
+	SS_t		<=	2'b01;
+	
+	#1
+	//CLR
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	DA_t		<=	3'b000;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b00000;
+	MD_t		<=	5'b00100;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//POP
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	DA_t		<=	3'b000;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	MD_t		<=	5'b10000;
+	MW_t		<=	0;
+	SS_t		<=	2'b10;
+	
+	#1
+	//SET
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	DA_t		<=	3'b000;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b01111;
+	Cin_t		<=	0;
+	MD_t		<=	5'b00100;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//NOT
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b100;
+	DA_t		<=	3'b101;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b10001;
+	MD_t		<=	5'b00100;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//AND
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b000;
+	BA_t		<=	3'b001;
+	DA_t		<=	3'b110;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b01000;
+	MD_t		<=	5'b00100;
+	MA_t		<=	0;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//OR
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b000;
+	BA_t		<=	3'b001;
+	DA_t		<=	3'b110;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b01110;
+	MD_t		<=	5'b00100;
+	MA_t		<=	0;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//OR
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b000;
+	BA_t		<=	3'b001;
+	DA_t		<=	3'b110;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b00110;
+	MD_t		<=	5'b00100;
+	MA_t		<=	0;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//MOVA
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b000;
+	DA_t		<=	3'b111;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b01100;
+	MD_t		<=	5'b00100;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//MOVB
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	BA_t		<=	3'b001;
+	DA_t		<=	3'b010;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b01010;
+	MD_t		<=	5'b00100;
+	MA_t		<=	0;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//ADDI
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b000;
+	DA_t		<=	3'b000;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b10100;
+	Cin_t		<=	0;
+	MD_t		<=	5'b00100;
+	MA_t		<=	1;
+	k_t		<=	4'h00f0;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//SUBI
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b000;
+	DA_t		<=	3'b111;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b10110;
+	Cin_t		<=	1;
+	MD_t		<=	5'b00100;
+	MA_t		<=	1;
+	k_t		<=	4'h00f0;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//ANDI
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b101;
+	DA_t		<=	3'b000;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b01000;
+	MD_t		<=	5'b00100;
+	MA_t		<=	1;
+	k_t		<=	4'h00f4;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//ORI
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b010;
+	DA_t		<=	3'b000;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b01110;
+	MD_t		<=	5'b00100;
+	MA_t		<=	1;
+	k_t		<=	4'hf000;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//XORI
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	AA_t		<=	3'b000;
+	DA_t		<=	3'b000;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	FS_t		<=	5'b00110;
+	MD_t		<=	5'b00100;
+	MA_t		<=	1;
+	k_t		<=	4'h0fbb;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	#1
+	//STI
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	BA_t		<=	3'b000;
+	WR_t		<=	0;
+	clear_t	<=	0;
+	MD_t		<=	5'b01000;
+	MA_t		<=	1;
+	k_t		<=	4'h0000;
+	MW_t		<=	1;
+	SS_t		<=	2'b00;
+	
+	#1
+	//LDI
+	PS_t		<=	2'b01;
+	IR_L_t	<=	1;
+	DA_t		<=	3'b001;
+	WR_t		<=	1;
+	clear_t	<=	0;
+	MD_t		<=	5'b01000;
+	MA_t		<=	1;
+	k_t		<=	4'h0000;
+	MW_t		<=	0;
+	SS_t		<=	2'b00;
+	
+	$stop 
+	end 
+endmodule
+		
